89586d9981aee7f231039c46bffb0ed7f2d3c8b0
Datior <leonardowederveiga@yahoo.com>
2023-05-29 14:00:05 -0300

    Relação e associações

---
+++ api/models/matriculas.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Matriculas = sequelize.define('Matriculas', {
+    status: DataTypes.STRING
+  }, {});
+  Matriculas.associate = function(models) {
+    Matriculas.belongTo(models.Pessoas, {
+      foreignKey: 'estudante_id'
+    })
+    Matriculas.belongTo(models.Turmas, { 
+      foreignKey: 'turma_id' 
+    })
+  };
+  return Matriculas;
+};
